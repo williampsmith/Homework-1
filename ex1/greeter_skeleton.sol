0ea213762952076b938eebcd497fa3aceb4ec61c
William Smith <williampsmith@berkeley.edu>
2017-09-11 21:49:26 -0700

    completed hw assignments

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -4,12 +4,17 @@ pragma solidity ^0.4.13;
 contract greeter {
 	/* Add one variable to hold our greeting */
 	string greeting;
+	uint public callCount;
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
+		greeting = _greeting;
+		callCount = 0;
 	}
 
 	function greet() constant returns (string)  {
 		/* Write one line of code to allow the contract to return our greeting */
+		callCount += 1;
+		return greeting;
 	}
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,8 +2,32 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+	mapping(uint => uint) fibs;
+	event Calculated(uint position, uint result);
+
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+		if (position <= 1) {
+			return position;
+		}
+
+		if (fibs[position] != 0) {
+	        return fibs[position];
+	    }
+
+		uint i = 1;
+		uint last = 0;
+		result = 1;
+
+		while (i < position) {
+			uint next = last + result;
+			last = result;
+			result = next;
+			i++;
+		}
+
+		fibs[position] = result;
+		Calculated(position, result);
+		return result;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
